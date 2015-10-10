ecb8911892db57b45aeb836779fc9a4cdfb6eac9
Alessandra Caroline Faisst <caroline.faisst@gmail.com>
2015-10-09 19:06:52 -0700

    delete tracks controller, add trackpicks controller, change migrations to user reference, change nested routes from tracks to trackpicks

---
+++ app/controllers/trackpicks_controller.rb
@@ -0,0 +1,2 @@
+class TrackpicksController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :models
 
   resources :playlists do
-    resources :tracks
+    resources :trackpicks
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
