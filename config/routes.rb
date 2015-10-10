23ce323f750308fcf90ce28fd5291460de7c6267
sstern6 <scott.stern06@gmail.com>
2015-10-09 17:43:00 -0700

    Add devise_for users in routes and add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  devise_for :users, controllers: { sessions: "users/sessions" }
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
