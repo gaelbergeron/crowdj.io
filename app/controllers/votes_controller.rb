83f31cc87f5881a9ae60c2a027179cf4b85092fb
Alessandra Caroline Faisst <caroline.faisst@gmail.com>
2015-10-12 15:19:19 -0700

    change on playlist votes listener

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,6 @@
 class VotesController < ApplicationController
 
   def create
-
     @trackpick = Trackpick.where(:id => params[:trackpick]).first
     @playlist = @trackpick.playlist
     @vote = Vote.create(:trackpick_id => params[:trackpick],:user_id => current_user.id, value: params[:value])
