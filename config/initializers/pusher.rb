957f54c51d622b727089981e3929e099aaa4d75a
Alessandra Caroline Faisst <caroline.faisst@gmail.com>
2015-10-11 17:50:59 -0700

    clean up pusher initializer;

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -2,8 +2,6 @@ require 'pusher'
 
 # Pusher.logger = Rails.logger
 
-# Pusher.url
-
 # pusher = Pusher::Client.new app_id: '147240', key: 'd4568fcc3382d859b1b4', secret: 'beb94a9e92c3bf576f8f'
 
 Pusher.app_id = '147240'
