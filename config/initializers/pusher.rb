1eced9896465e60d915869b96786d73ec4dc18fc
Alessandra Caroline Faisst <caroline.faisst@gmail.com>
2015-10-14 23:00:17 -0700

    change order of pusher url

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,9 +1,9 @@
 require 'pusher'
 
+Pusher.url = ENV['PUSHER_URL']
+
 Pusher.app_id = ENV['PUSHER_ID']
 Pusher.key = ENV['PUSHER_KEY']
 Pusher.secret = ENV['PUSHER_SECRET']
 
-# Pusher.url = ENV['PUSHER_URL']
-
-# Pusher.logger = Rails.logger
+Pusher.logger = Rails.logger
