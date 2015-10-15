8e1316f06d0d2b80c2609b9ae8fcd30fd62c67d4
gaelbergeron <gael.bergeron@gmail.com>
2015-10-15 12:36:46 -0700

    Add search and results instructions

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -3,7 +3,5 @@ require 'pusher'
 Pusher.app_id = ENV['PUSHER_ID']
 Pusher.key = ENV['PUSHER_KEY']
 Pusher.secret = ENV['PUSHER_SECRET']
-
 Pusher.url = ENV['PUSHER_URL']
-
 Pusher.logger = Rails.logger
