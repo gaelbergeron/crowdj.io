2b2757a4944e6042b22d2cde75d8a122f7f51b94
Alessandra Caroline Faisst <caroline.faisst@gmail.com>
2015-10-14 22:42:46 -0700

    comment pusher logger and pusher url

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -4,6 +4,6 @@ Pusher.app_id = ENV['PUSHER_ID']
 Pusher.key = ENV['PUSHER_KEY']
 Pusher.secret = ENV['PUSHER_SECRET']
 
-Pusher.url = ENV['PUSHER_URL']
+# Pusher.url = ENV['PUSHER_URL']
 
-Pusher.logger = Rails.logger
+# Pusher.logger = Rails.logger
