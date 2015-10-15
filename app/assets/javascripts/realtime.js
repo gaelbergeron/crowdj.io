edf8079f282b615f27f534de8370746c758cb268
Alessandra Caroline Faisst <caroline.faisst@gmail.com>
2015-10-14 21:41:36 -0700

    merge code into master and setup keys for production

--- app/assets/javascripts/realtime.js
+++ app/assets/javascripts/realtime.js
@@ -1,6 +1,6 @@
 $(document).on("crowdj:playlists_show", function(){
   var playlistId = $('.current_playlist').attr('id')
-  var pusher = new Pusher('d4568fcc3382d859b1b4')
+  var pusher = new Pusher('896f53585d4e20c5421d')
   var channel =  pusher.subscribe('playlist'+playlistId)
 
   channel.bind('add_trackpick', function(data) {
